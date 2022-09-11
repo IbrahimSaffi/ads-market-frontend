8f86541bd31e580803d8ff57f0440c78b6766017
IbrahimSaffi <ibrahimsaffinew@gmail.com>
2022-09-11 18:37:42 +0500

    commit

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -67,9 +67,9 @@ export default function SignInSide() {
               alignItems: 'center',
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
               <LockOutlinedIcon />
-            </Avatar>
+            </Avatar> */}
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
